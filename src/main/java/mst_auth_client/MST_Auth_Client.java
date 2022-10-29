6ca5c38dbad5b0b7fa89efbbc71cfb3232217db9
mlb12-illinois <mlb12@illinois.edu>
2022-10-29 16:53:07 -0500

    Added MSTAException
    Added MSTAException

--- src/main/java/mst_auth_client/MST_Auth_Client.java
+++ src/main/java/mst_auth_client/MST_Auth_Client.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mst_auth_library.MSTAException;
 import mst_auth_library.MST_Auth_Servlet;
 
 public class MST_Auth_Client {
@@ -16,7 +17,7 @@ public class MST_Auth_Client {
 	public void SetLibrary (MST_Auth_Servlet MSTALibrary ) {
 		msta_library = MSTALibrary;			
 	}
-	public void doGet(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException, MSTAException {
 		msta_library.SetMicroservice("MSTAAuthorization");
 		msta_library.SetMethodWithBodyString("GET", "MSTAAuthorization OY a get");
 		msta_library.SetHeader("Content-Type", "application/json; utf-8");
@@ -29,13 +30,13 @@ public class MST_Auth_Client {
 		HttpResponse resp2 = msta_library.SendRequest();	  
 	    response.getWriter().append(resp2.body().toString());
 	}
-	public void doPost(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException, MSTAException {
 	    response.getWriter().append("doPut Served at: ").append(request.getContextPath());
 	}
-	public void doPut(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException {
+	public void doPut(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException, MSTAException {
 	    response.getWriter().append("doPut Served at: ").append(request.getContextPath());
 	}
-	public void doDelete(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException {
+	public void doDelete(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws ServletException, IOException, MSTAException {
 	    response.getWriter().append("doDelete Served at: ").append(request.getContextPath());
 	}
 }
