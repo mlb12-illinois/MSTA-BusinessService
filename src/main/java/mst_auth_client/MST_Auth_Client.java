f45d5b636ec9cc3461ec4688c18676c216aa90a1
mlb12-illinois <mlb12@illinois.edu>
2022-11-05 11:15:36 -0500

    Pilot version
    Pilot version

--- src/main/java/mst_auth_client/MST_Auth_Client.java
+++ src/main/java/mst_auth_client/MST_Auth_Client.java
@@ -3,6 +3,7 @@ package mst_auth_client;
 import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +25,11 @@ public class MST_Auth_Client {
 		msta_library = MSTALibrary;			
 	}
 	public void doGet(HttpServletRequest request, HttpServletResponse response, String trustedbody) throws IOException, MSTAException {
+		//System.out.println("Business doPost");
+		String input;
+		if (trustedbody == null)
+			input = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+		else input = trustedbody;
 		 //System.out.println("CLIENT IN");
 		// simulate a little work
 		try {
@@ -36,18 +42,20 @@ public class MST_Auth_Client {
 		msta_library.SetMicroservice("MSTAAuthorization");
 		//msta_library.SetMicroservice("MSTADataProvider");
 		//msta_library.SetMicroservice("http://localhost:8080/MSTA-AuthorizationService/MSTAAuthorization.html");
-		msta_library.SetMethodWithBodyString("GET", "MSTAAuthorization OY a get");
+		msta_library.SetMethodWithBodyString("GET", input);
 		msta_library.SetHeader("Content-Type", "application/json; utf-8");
 		HttpResponse<String> resp = msta_library.SendRequest();	  
 		// System.out.println("CLIENT SENDING");
 	    response.getWriter().append(resp.body().toString());
+	    //System.out.println(resp.body().toString());
 		// System.out.println("CLIENT 1 SENT");
 
 		msta_library.SetMicroservice("MSTADataProvider");
 		//msta_library.SetMicroservice("http://localhost:8080/MSTA-DataProvider/MSTADataProvider.html");
-		msta_library.SetMethodWithBodyString("GET", "MSTADataProvider OY a get");
+		msta_library.SetMethodWithBodyString("GET", input);
 		msta_library.SetHeader("Content-Type", "application/json; utf-8");
 		HttpResponse<String> resp2 = msta_library.SendRequest();	  
+	    //System.out.println(resp2.body().toString());
 	    response.getWriter().append(resp2.body().toString());
 	    //System.out.println("CLIENT 2 SENT");
 	}
